517bfe59d8fc6aeeaad0fbb97630fecf844a2ecf
diegoxgarcia <diegoxgarcia@gmail.com>
2015-09-25 00:25:26 -0300

    Modificacion DB Adapter

--- app/src/main/java/com/example/diegox101/DBAdapter/ClimaDBAdapter.java
+++ app/src/main/java/com/example/diegox101/DBAdapter/ClimaDBAdapter.java
@@ -1,7 +1,29 @@
 package com.example.diegox101.DBAdapter;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.diegox101.sqlitedb.DBHelper;
+
+import java.sql.SQLException;
+
 /**
  * Created by diegoxgarcia on 24/9/15.
  */
 public class ClimaDBAdapter {
+
+    private Context context;
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
+
+
+    public ClimaDBAdapter(Context context) {
+        this.context = context;
+    }
+
+    public ClimaDBAdapter abrir() throws SQLException{
+        dbHelper = new DBHelper(this.context);
+        db = dbHelper.getWritableDatabase();
+        return this;
+    }
 }
